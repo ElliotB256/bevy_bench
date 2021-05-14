2535db990d512fd33b139cec2caeee7e7d096929
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2021-05-14 11:59:39 +0100

    parallel systems

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate bevy;
 
 use bevy::app::App;
-use bevy::prelude::*;
+use bevy::{prelude::*, tasks::prelude::*};
 
 extern crate nalgebra;
 use nalgebra::Vector3;
 
 pub struct Position(Vector3<f64>);
 pub struct Velocity(Vector3<f64>);
-#[derive(Copy,Clone)]
+#[derive(Copy, Clone)]
 pub struct Force(Vector3<f64>);
 pub struct OldForce(Force);
 pub struct Mass(f64);
@@ -17,20 +17,27 @@ pub struct Timestep {
     pub dt: f64,
 }
 
-fn integrate_position(mut query: Query<(&Velocity, &Mass, &Force, &mut Position, &mut OldForce)>, timestep: Res<Timestep>) {
+fn integrate_position(
+    pool: Res<ComputeTaskPool>,
+    mut query: Query<(&Velocity, &Mass, &Force, &mut Position, &mut OldForce)>,
+    timestep: Res<Timestep>,
+) {
     let dt = timestep.dt;
-    for (vel, mass, force, mut pos, mut old_force) in query.iter_mut() {
+    query.par_for_each_mut(&pool, 32, |(vel, mass, force, mut pos, mut old_force)| {
         pos.0 = pos.0 + vel.0 * dt + force.0 / (mass.0) / 2.0 * dt * dt;
-		old_force.0 = *force;
-    }
+        old_force.0 = *force;
+    });
 }
 
-fn integrate_velocity(mut query: Query<(&mut Velocity, &Force, &OldForce, &Mass)>, timestep: Res<Timestep>) {
+fn integrate_velocity(
+    pool: Res<ComputeTaskPool>,
+    mut query: Query<(&mut Velocity, &Force, &OldForce, &Mass)>,
+    timestep: Res<Timestep>,
+) {
     let dt = timestep.dt;
-    for (mut vel, force, old_force, mass) in query.iter_mut() {
-    vel.0 = vel.0
-    + (force.0 + old_force.0.0) / (mass.0) / 2.0 * dt;
-    }
+    query.par_for_each_mut(&pool, 32, |(mut vel, force, old_force, mass)| {
+        vel.0 = vel.0 + (force.0 + old_force.0 .0) / (mass.0) / 2.0 * dt;
+    });
 }
 
 fn spawn_atoms(mut commands: Commands) {
@@ -38,19 +45,19 @@ fn spawn_atoms(mut commands: Commands) {
     for _ in 1..10_000 {
         commands.spawn().insert_bundle((
             Position {
-                0: Vector3::new(0.0,0.0,0.0)
+                0: Vector3::new(0.0, 0.0, 0.0),
             },
             Velocity {
-                0: Vector3::new(0.2,0.5,1.0)
+                0: Vector3::new(0.2, 0.5, 1.0),
             },
         ));
     }
 }
 
-fn harmonic_trap(mut query: Query<(&mut Force, &Position)>) {
-    for (mut force, pos) in query.iter_mut() {
+fn harmonic_trap(pool: Res<ComputeTaskPool>, mut query: Query<(&mut Force, &Position)>) {
+    query.par_for_each_mut(&pool, 32, |(mut force, pos)| {
         force.0 = -pos.0;
-    }
+    });
 }
 
 fn simple_runner(mut app: App) {
@@ -71,4 +78,3 @@ fn main() {
         .set_runner(simple_runner)
         .run();
 }
-
